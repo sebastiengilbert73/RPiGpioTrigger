f94c026d21f3a926d4750c8f1ca55829f8852fc1
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2016-04-23 16:01:43 -0400

    Add config file RPiGpioTriggerMainConfig.xml RPiGpioTriggerMain.cpp: open config file

--- RPiGpioTriggerMain.cpp
+++ RPiGpioTriggerMain.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <tinyxml.h>
 #include <string>
+#include <sstream>
 #include <log4cxx/logger.h>
 #include <log4cxx/xml/domconfigurator.h>
+#include <stdexcept> // runtime_error
 
 
 using namespace std;
@@ -15,17 +17,40 @@ LoggerPtr loggerPtr(Logger::getLogger("RPiGpioTriggerMain"));
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	try
 	{
-		cout << "Usage: RPiGpioTriggerMain [configFilename]" << endl;
-		return -1;
+		if (argc < 2)
+		{
+			cout << "Usage: RPiGpioTriggerMain [configFilename]" << endl;
+			return -1;
+		}
+	
+		DOMConfigurator::configure("RPiGpioTriggerMainLog4cxxConfig.xml");
+		LOG4CXX_DEBUG(loggerPtr, "main()");
+	
+		// Read config file
+		string configFilename = argv[1];
+		TiXmlDocument configDoc(configFilename);
+		if (!configDoc.LoadFile())
+		{
+			stringstream msg;
+			msg << "main(): Could not load configuration file '" << configFilename << "'";
+			LOG4CXX_ERROR(loggerPtr, msg.str());
+			throw runtime_error(msg.str().c_str());
+		}
+		TiXmlElement* rootElmPtr = configDoc.FirstChildElement("RPiGPioTriggerMain");
+		if (rootElmPtr == NULL)
+		{
+			stringstream msg;
+			msg << "main(): Could not find XML element 'RPiGPioTriggerMain' at the root of file '" << configFilename << "'";
+			LOG4CXX_ERROR(loggerPtr, msg.str());
+			throw runtime_error(msg.str().c_str());
+		}
+	
+		cout << "Done!" << endl;
 	}
-	
-	DOMConfigurator::configure("RPiGpioTriggerMainLog4cxxConfig.xml");
-	LOG4CXX_DEBUG(loggerPtr, "main()");
-	
-	// Read config file
-	string configFilename = argv[1];
-	
-	cout << "Done!" << endl;
+	catch (runtime_error e)
+	{
+		cout << "Caught exception: " << e.what() << endl;
+	}
 }
