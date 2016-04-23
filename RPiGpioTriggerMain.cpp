82794a4bdb4ba5dbe45b188a64260c475d8a6757
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2016-04-23 13:29:14 -0400

    Add RPiGpioTriggerConfig.xml Skeleton for main

--- RPiGpioTriggerMain.cpp
+++ RPiGpioTriggerMain.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <tinyxml.h>
+#include <string>
 
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "argc = " << argc << endl;
+	if (argc < 2)
+	{
+		cout << "Usage: RPiGpioTriggerMain [configFilename]" << endl;
+		return -1;
+	}
+	// Read config file
+	string configFilename = argv[1];
+	
+	cout << "Done!" << endl;
+}
