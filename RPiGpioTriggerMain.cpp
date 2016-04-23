9b3d7ac3c19916277232848848954220a5848545
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2016-04-23 15:35:37 -0400

    Add support for log4cxx Add log4cxx config file

--- RPiGpioTriggerMain.cpp
+++ RPiGpioTriggerMain.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <tinyxml.h>
 #include <string>
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+
 
 using namespace std;
+using namespace log4cxx;
+using namespace log4cxx::xml;
+using namespace log4cxx::helpers;
+
+// Define static logger variable
+LoggerPtr loggerPtr(Logger::getLogger("RPiGpioTriggerMain"));
 
 int main(int argc, char **argv)
 {
-	cout << "argc = " << argc << endl;
 	if (argc < 2)
 	{
 		cout << "Usage: RPiGpioTriggerMain [configFilename]" << endl;
 		return -1;
 	}
+	
+	DOMConfigurator::configure("RPiGpioTriggerMainLog4cxxConfig.xml");
+	LOG4CXX_DEBUG(loggerPtr, "main()");
+	
 	// Read config file
 	string configFilename = argv[1];
 	
